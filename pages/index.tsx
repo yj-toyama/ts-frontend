0ad5d0a82aea686d3c34d5c20d6c80263e9c5985
yj-toyama <yuji.toyama@gmail.com>
2023-05-06 14:41:37 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQuery, useMutation, gql } from '@apollo/client';
 
 const HELLO_QUERY = gql`
@@ -16,16 +17,18 @@ const DUMMY_MUTATION = gql`
 export default function Home() {
   const { loading, error, data } = useQuery(HELLO_QUERY);
   const [performDummyMutation, { data: mutationData }] = useMutation(DUMMY_MUTATION);
+  const [showBackend1, setShowBackend1] = useState(false);
+  const [showBackend2, setShowBackend2] = useState(false);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   const handleClick1 = () => {
-    console.log(data.helloFromBackend1);
+    setShowBackend1(true);
   };
 
   const handleClick2 = () => {
-    console.log(data.helloFromBackend2);
+    setShowBackend2(true);
   };
 
   const handleDummyMutationClick = async () => {
@@ -34,10 +37,10 @@ export default function Home() {
 
   return (
     <div>
-      <button onClick={handleClick1}>call backend1</button>
-      {data && <h2>{data.helloFromBackend1}</h2>}
-      <button onClick={handleClick2}>call backend2</button>
-      {data && <h2>{data.helloFromBackend2}</h2>}
+      <button onClick={handleClick1}>Get Hello From Backend1</button>
+      <button onClick={handleClick2}>Get Hello From Backend2</button>
+      {showBackend1 && <h2>{data.helloFromBackend1}</h2>}
+      {showBackend2 && <h2>{data.helloFromBackend2}</h2>}
       <button onClick={handleDummyMutationClick}>Perform Dummy Mutation</button>
       {mutationData && <h2>{mutationData.dummyMutation}</h2>}
     </div>
